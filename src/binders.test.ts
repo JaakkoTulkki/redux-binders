0f0bd914bf5c9f1d0fe86a9a6ad1915cba5266fb
Jaakko <jaakko.tulkki@gmail.com>
2019-02-09 22:04:09 +0000

    Changed indentation.

--- src/binders.test.ts
+++ src/binders.test.ts
@@ -2,9 +2,9 @@ import {createStore, combineReducers, applyMiddleware, AnyAction} from 'redux';
 import thunk from 'redux-thunk';
 
 import {
-    AnyActionFunction, BindableAction,
-    bindActionToScope,
-    bindReducerToScope,
+  AnyActionFunction, BindableAction,
+  bindActionToScope,
+  bindReducerToScope,
 } from './index';
 
 const scope = 'myscope';
@@ -12,114 +12,115 @@ const otherScope = 'otherScope';
 const ACTION_TYPE = 'CHANGE';
 
 function actionCreator(payload) {
-    return {
-        type: ACTION_TYPE,
-        payload,
-    }
+  return {
+    type: ACTION_TYPE,
+    payload,
+  }
 }
 
 function thunkActionCreator(payload, prop) {
-    return function thunkAction(dispatch, getState, extra) {
-        const state = getState()[prop];
-        const contrivedPayload = state + payload + extra;
-        dispatch({type: ACTION_TYPE, payload: contrivedPayload});
-    }
+  return function thunkAction(dispatch, getState, extra) {
+    const state = getState()[prop];
+    const contrivedPayload = state + payload + extra;
+    dispatch({type: ACTION_TYPE, payload: contrivedPayload});
+  }
 }
 
 function asynThunkActionCreator(payload) {
-    return function thunkAction(dispatch, getState, extra) {
-        return new Promise((resolve) => {
-            dispatch(actionCreator(payload));
-            resolve(payload);
-        });
-    }
+  return function thunkAction(dispatch, getState, extra) {
+    return new Promise((resolve) => {
+      dispatch(actionCreator(payload));
+      resolve(payload);
+    });
+  }
 }
 
 
 function reducer(state = '', action: AnyAction) {
-    if (action.type === ACTION_TYPE) {
-        return action.payload;
-    }
-    return state;
+  if (action.type === ACTION_TYPE) {
+    return action.payload;
+  }
+  return state;
 }
+
 interface StateShape {
-    scoped: any;
-    nonScoped: any;
-    differentScoped: any;
+  scoped: any;
+  nonScoped: any;
+  differentScoped: any;
 }
 
 type ExtraArg = string;
 
 const rootReducer = combineReducers({
-    scoped: bindReducerToScope(reducer, scope),
-    nonScoped: reducer,
-    differentScoped: bindReducerToScope(reducer, otherScope),
+  scoped: bindReducerToScope(reducer, scope),
+  nonScoped: reducer,
+  differentScoped: bindReducerToScope(reducer, otherScope),
 });
 
 describe('bindActionToScope with', () => {
-    let store: any;
-    beforeEach(() => {
-        store = createStore(rootReducer, undefined, applyMiddleware(thunk.withExtraArgument('myExtra')))
+  let store: any;
+  beforeEach(() => {
+    store = createStore(rootReducer, undefined, applyMiddleware(thunk.withExtraArgument('myExtra')))
+  });
+  it('store should have correct shape', () => {
+    expect(Object.keys(store.getState())).toEqual(['scoped', 'nonScoped', 'differentScoped'])
+  });
+
+  describe('plain actions', () => {
+    it('should change with bound and non-bound reducers', () => {
+      const boundAction = bindActionToScope({type: ACTION_TYPE, payload: 'first'}, scope) as AnyAction;
+      store.dispatch(boundAction);
+      expect(store.getState()['scoped']).toEqual('first');
+      expect(store.getState()['nonScoped']).toEqual('first');
+      expect(store.getState()['differentScoped']).toEqual('');
+
+      const boundActionToScope2 = bindActionToScope({type: ACTION_TYPE, payload: 'other'}, otherScope) as AnyAction;
+      store.dispatch(boundActionToScope2);
+      expect(store.getState()['scoped']).toEqual('first');
+      expect(store.getState()['nonScoped']).toEqual('other');
+      expect(store.getState()['differentScoped']).toEqual('other');
     });
-    it('store should have correct shape', () => {
-        expect(Object.keys(store.getState())).toEqual(['scoped', 'nonScoped', 'differentScoped'])
+  });
+
+  describe('action creators', () => {
+    it('should change with bound and non-bound reducers', () => {
+      const boundAction = bindActionToScope<undefined, StateShape, ExtraArg>(actionCreator, scope) as AnyActionFunction<undefined, StateShape, ExtraArg>;
+      store.dispatch(boundAction('one'));
+      expect(store.getState()['scoped']).toEqual('one');
+      expect(store.getState()['nonScoped']).toEqual('one');
+      expect(store.getState()['differentScoped']).toEqual('');
+
+
+      const boundActionToScope2 = bindActionToScope<undefined, StateShape, ExtraArg>(actionCreator, otherScope) as AnyActionFunction<undefined, StateShape, ExtraArg>;
+      store.dispatch(boundActionToScope2('two'));
+      expect(store.getState()['scoped']).toEqual('one');
+      expect(store.getState()['nonScoped']).toEqual('two');
+      expect(store.getState()['differentScoped']).toEqual('two');
+    });
+  });
+
+  describe('action creators that return thunks', () => {
+    it('should work with bound and non-bound reducers', () => {
+      const boundSimpleAction = bindActionToScope({type: ACTION_TYPE, payload: 'ok-'}, scope) as AnyAction;
+      store.dispatch(boundSimpleAction);
+
+      const boundAction = bindActionToScope<void, StateShape, ExtraArg>(thunkActionCreator, scope) as AnyActionFunction<undefined, StateShape, ExtraArg>;
+      store.dispatch(boundAction('one', 'scoped'));
+
+      expect(store.getState()['scoped']).toEqual('ok-onemyExtra');
+      expect(store.getState()['nonScoped']).toEqual('ok-onemyExtra');
+      expect(store.getState()['differentScoped']).toEqual('');
     });
 
-    describe('plain actions', () => {
-        it('should change with bound and non-bound reducers', () => {
-            const boundAction = bindActionToScope({type: ACTION_TYPE, payload: 'first'}, scope) as AnyAction;
-            store.dispatch(boundAction);
-            expect(store.getState()['scoped']).toEqual('first');
-            expect(store.getState()['nonScoped']).toEqual('first');
-            expect(store.getState()['differentScoped']).toEqual('');
-
-            const boundActionToScope2 = bindActionToScope({type: ACTION_TYPE, payload: 'other'}, otherScope) as AnyAction;
-            store.dispatch(boundActionToScope2);
-            expect(store.getState()['scoped']).toEqual('first');
-            expect(store.getState()['nonScoped']).toEqual('other');
-            expect(store.getState()['differentScoped']).toEqual('other');
-        });
+    it('should work with async thunks', async (done) => {
+      const boundAction = bindActionToScope<Promise<any>, StateShape, ExtraArg>(asynThunkActionCreator, scope) as AnyActionFunction<Promise<any>, StateShape, ExtraArg>;
+      store.dispatch(boundAction('hello world')).then((data: string) => {
+        expect(data).toEqual('hello world');
+        expect(store.getState().scoped).toEqual('hello world');
+        expect(store.getState().nonScoped).toEqual('hello world');
+        expect(store.getState().differentScoped).toEqual('');
+        done();
+      })
     });
-
-    describe('action creators', () => {
-        it('should change with bound and non-bound reducers', () => {
-            const boundAction = bindActionToScope<undefined, StateShape, ExtraArg>(actionCreator, scope) as AnyActionFunction<undefined, StateShape, ExtraArg>;
-            store.dispatch(boundAction('one'));
-            expect(store.getState()['scoped']).toEqual('one');
-            expect(store.getState()['nonScoped']).toEqual('one');
-            expect(store.getState()['differentScoped']).toEqual('');
-
-
-            const boundActionToScope2 = bindActionToScope<undefined, StateShape, ExtraArg>(actionCreator, otherScope) as AnyActionFunction<undefined, StateShape, ExtraArg>;
-            store.dispatch(boundActionToScope2('two'));
-            expect(store.getState()['scoped']).toEqual('one');
-            expect(store.getState()['nonScoped']).toEqual('two');
-            expect(store.getState()['differentScoped']).toEqual('two');
-        });
-    });
-
-    describe('action creators that return thunks', () => {
-        it('should work with bound and non-bound reducers', () => {
-            const boundSimpleAction = bindActionToScope({type: ACTION_TYPE, payload: 'ok-'}, scope) as AnyAction;
-            store.dispatch(boundSimpleAction);
-
-            const boundAction = bindActionToScope<void, StateShape, ExtraArg>(thunkActionCreator, scope) as AnyActionFunction<undefined, StateShape, ExtraArg>;
-            store.dispatch(boundAction('one', 'scoped'));
-
-            expect(store.getState()['scoped']).toEqual('ok-onemyExtra');
-            expect(store.getState()['nonScoped']).toEqual('ok-onemyExtra');
-            expect(store.getState()['differentScoped']).toEqual('');
-        });
-
-        it('should work with async thunks', async (done) => {
-            const boundAction = bindActionToScope<Promise<any>, StateShape, ExtraArg>(asynThunkActionCreator, scope) as AnyActionFunction<Promise<any>, StateShape, ExtraArg>;
-            store.dispatch(boundAction('hello world')).then((data: string) => {
-                expect(data).toEqual('hello world');
-                expect(store.getState().scoped).toEqual('hello world');
-                expect(store.getState().nonScoped).toEqual('hello world');
-                expect(store.getState().differentScoped).toEqual('');
-                done();
-            })
-        });
-    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import {ThunkAction} from "redux-thunk";
 
 export type Reducer<StateShape, Action=AnyAction> = (state: StateShape, action: Action) => StateShape;
 export type AnyActionFunction<ReturnValue, State, ExtraArg, Act extends AnyAction=AnyAction> = (...args: any[]) => AnyAction | ThunkAction<ReturnValue, State, ExtraArg, Act>;
-export type BindableAction<ReturnValue, State, ExtraArg, Act extends AnyAction> = AnyActionFunction<ReturnValue, State, ExtraArg, Act> | AnyAction;
+export type BindableAction<ReturnValue, State, ExtraArg, Act extends AnyAction> =
+  AnyActionFunction<ReturnValue, State, ExtraArg, Act>
+  | AnyAction;
 
 const scoper = '__scope';
+
 function bindActionCreatorToScope(creator, scope) {
   return function wrappedActionCrator(...args) {
     const action = creator(...args);
@@ -16,15 +19,17 @@ function bindActionCreatorToScope(creator, scope) {
     if (typeof action === "function") {
       return function thunk(dispatch, getState, rest) {
         function boundDispatch(actionFromThunk) {
-            dispatch(bindActionToScope(actionFromThunk, scope))
+          dispatch(bindActionToScope(actionFromThunk, scope))
         }
+
         return action(boundDispatch, getState, rest)
       }
     }
   }
 }
-export function bindActionToScope<ReturnValue, State, ExtraArg, Act extends AnyAction = AnyAction>(action: AnyAction | AnyActionFunction<ReturnValue, State, ExtraArg, Act>, scope: string): AnyAction | AnyActionFunction<ReturnValue, State, ExtraArg, Act>{
-  if (typeof action !== "function" ) {
+
+export function bindActionToScope<ReturnValue, State, ExtraArg, Act extends AnyAction = AnyAction>(action: AnyAction | AnyActionFunction<ReturnValue, State, ExtraArg, Act>, scope: string): AnyAction | AnyActionFunction<ReturnValue, State, ExtraArg, Act> {
+  if (typeof action !== "function") {
     return {...action, ...{[scoper]: {scope}}} as AnyAction;
   } else {
     return bindActionCreatorToScope(action, scope) as AnyActionFunction<ReturnValue, State, ExtraArg, Act>;
@@ -33,10 +38,10 @@ export function bindActionToScope<ReturnValue, State, ExtraArg, Act extends AnyA
 
 export function bindReducerToScope<StateShape, Action=AnyAction>(reducer: Reducer<StateShape, Action>, scope: string) {
   return function boundReducer(state, action) {
-    if(state === undefined) {
+    if (state === undefined) {
       return reducer(undefined, {} as Action);
     }
-    if(action[scoper] && action[scoper].scope === scope) {
+    if (action[scoper] && action[scoper].scope === scope) {
       return reducer(state, action);
     }
     return state;
@@ -44,14 +49,14 @@ export function bindReducerToScope<StateShape, Action=AnyAction>(reducer: Reduce
 }
 
 export function scopedCombineReducers<StateShape>(reducers: ReducersMapObject<StateShape, any>, scope: string): Reducer<StateShape, AnyAction> {
-    const objKeys = Object.keys(reducers);
-    const finalObj: ReducersMapObject = {};
-    for (let i = 0; i < objKeys.length; i++) {
-        const key = objKeys[i];
-        const reducer = reducers[key];
-        if (typeof reducer === 'function') {
-            finalObj[key] = bindReducerToScope(reducer, scope);
-        }
+  const objKeys = Object.keys(reducers);
+  const finalObj: ReducersMapObject = {};
+  for (let i = 0; i < objKeys.length; i++) {
+    const key = objKeys[i];
+    const reducer = reducers[key];
+    if (typeof reducer === 'function') {
+      finalObj[key] = bindReducerToScope(reducer, scope);
     }
-    return combineReducers<StateShape>(finalObj);
+  }
+  return combineReducers<StateShape>(finalObj);
 }
